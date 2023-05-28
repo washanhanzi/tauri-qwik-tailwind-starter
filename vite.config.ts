51c1cfc2b6fd95a7f820a793cccedc97592013a6
jingyu <francismajere@gmail.com>
2023-05-28 21:46:36 +0800

    FEAT update qwik to 1.1.4

--- vite.config.ts
+++ vite.config.ts
@@ -11,15 +11,5 @@ export default defineConfig(() => {
         'Cache-Control': 'public, max-age=600',
       },
     },
-    clearScreen: false,
-    envPrefix: ["VITE_", "TAURI_"],
-    build: {
-      // Tauri supports es2021
-      target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-      // don't minify for debug builds
-      minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-      // produce sourcemaps for debug builds
-      sourcemap: !!process.env.TAURI_DEBUG,
-    },
   };
 });
